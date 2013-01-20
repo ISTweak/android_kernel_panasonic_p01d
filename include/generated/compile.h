d2d2b1124745137641d4271364b6f13c81fff0f6
ISTweak <istweak@d-o-b.org>
2013-01-20 15:12:07 +0900

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 4 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 PREEMPT Mon Jan 14 13:50:40 JST 2013"
+#define LINUX_COMPILE_TIME "13:50:40"
+#define LINUX_COMPILE_BY "love"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
